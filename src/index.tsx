e08339e7dae3e5b68b786e67b4cd4e4b8af9a8e1
Timothy1102 <thang.vu@kobizo.de>
2022-12-06 02:01:17 +0700

    install solana libs, tailwindcss,...

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-
 import View from 'view'
-
 import reportWebVitals from 'reportWebVitals'
-
 import 'static/styles/index.less'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { PhantomWalletAdapter, Coin98WalletAdapter } from '@solana/wallet-adapter-wallets'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import configs from 'configs'
+
+const {
+    rpc: { endpoint },
+} = configs
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <View />
-  </BrowserRouter>,
+    <BrowserRouter>
+        <ConnectionProvider endpoint={endpoint}>
+			<WalletProvider wallets={[new PhantomWalletAdapter(), new Coin98WalletAdapter()]} autoConnect>
+				<WalletModalProvider>
+					<View />
+				</WalletModalProvider>
+			</WalletProvider>
+        </ConnectionProvider>
+    </BrowserRouter>,
 )
 
 // If you want to start measuring performance in your app, pass a function
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,jsx,tsx}'],
+    theme: {
+        container: {
+            center: true,
+            padding: '1.5rem',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+        extend: {},
+    },
+    plugins: [],
+}
