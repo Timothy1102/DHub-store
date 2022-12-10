bf537910ce6989abdcfe13b263d1b3424f1aba6e
Timothy1102 <thang.vu@kobizo.de>
2022-12-10 17:18:53 +0700

    Update dApp metadata

--- src/view/assets/data/data.js
+++ src/view/assets/data/data.js
@@ -49,7 +49,7 @@ export const DAPP__DATA = [
 			metadata: {
 				title: 'ArthSwap',
 				description: 'What is ArthSwap? ArthSwap is the No.1 decentralized exchange platform with the largest community on Astar Network. With simple UI and design for High APY, it enables High-Frequency Trade on Astar Network. ArthSwap history ArthSwap was launched on January 22, 2022 as a first Dapps on Astar Network. In a short span of one month, ArthSwap has exceeded 90 million in its TVL and the number of its community users has expanded to more than 200K, users on Twitter, Discord and Telegram included. ArthSwap has received support from various backers. These include Next Web Capital, led by Astar Founder Sota, and OKX Blockdream Ventures. Key Features ArthSwap is a one-stop defi solution on Astar Network and will offer everything you want in defi. This includes Swap, Pool, Farming, Staking and IDO Launchpad.',
-				extra: 'Loan',
+				extra: 'IDO, launchpad',
 				media: 'https://nftstorage.link/ipfs/bafybeid5ftqbr2c3nnvwmgnt4gjnzrbetrnqsgn6tv24nojgscdplg5hzq/Screen%20Shot%202022-06-14%20at%2007.14.04.png',
 			},
 		},
@@ -63,8 +63,8 @@ export const DAPP__DATA = [
 		itemData: {
 			metadata: {
 				title: 'OnFinality',
-				description: 'description',
-				extra: 'extra',
+				description: `Project Overview OnFinality, a leading blockchain IaaS (infrastructure as a service) platform, is providing a reliable and scalable infrastructure to Astar and Shiden. The Astar community can now build applications on high-performance RPC archive nodes in the Shiden Network (and future Astar Network) for free. OnFinality's API Service is free for the Astar and Shiden community (within fair usage limits) and is built with scalability in mind. You can access reliable enterprise level API endpoints for Astar and Shiden in only minutes. OnFinality also provides a one-click-deploy node service that allows customers to deploy their own Astar and Shiden collator nodes in our platform. OnFinality is a SaaS platform that provides infrastructure and developer tools that saves developers hours and allows teams to grow with confidence. Our mission is to help blockchain and dApp developers build the decentralised future faster. Service This service is currently online and can be connected to via these endpoints`,
+				extra: 'Defi, CEX',
 				media: 'https://bafybeihzfflyc6tfleumoraezxbkstxq5stgojfh2ipd4kh2easrjza5fu.ipfs.nftstorage.link/Screen%20Shot%202022-06-14%20at%2016.30.22.png',
 			},
 		},
@@ -78,8 +78,8 @@ export const DAPP__DATA = [
 		itemData: {
 			metadata: {
 				title: 'NFT Creator',
-				description: 'description',
-				extra: 'extra',
+				description: `access to various tokens on Astar Network either from big projects or indie one. Our goal is to make reachable the possibility for users to swap tokens quickly and easily, and fairly reward Liquidity Providers with 0.25% transaction fees and DAO Governance Tokens ($ASX). This one is distributed by yield farming without lock-up, nor deposit/withdrawal fee. Key Features To take advantage of our participation to dApp Staking, we decided not to keep rewards but to use 100% of them to buy-back and burn $ASX tokens, in order to increase its value and ensure that users can benefit of it, even if they don't want to vote for DAO proposals.`,
+				extra: 'DEX, Exchange, Other',
 				media: 'https://bafybeidumk7uszwxoijntdgh6iiyt5lzvmqarbqblxcekbcpwv2h5svd4a.ipfs.nftstorage.link/Screen%20Shot%202022-06-14%20at%2013.51.27.png',
 			},
 		},
@@ -93,8 +93,8 @@ export const DAPP__DATA = [
 		itemData: {
 			metadata: {
 				title: 'Astar Degens',
-				description: 'description',
-				extra: 'extra',
+				description: `Astar Degen's Mission    Incubating promising projects and bringing new ideas to the Astar Ecosystem with the help of a community-governed DAO fund.     Astar Degens is a community without hierarchy. Where impactful action is incentivized and rewarded. We welcome all ideas equally, and value productive effort. As a community, we help realize the value of supportive cooperation within the blockchain space, by encouraging fearless participation within the Astar Network.   We aim to maximize both utility and transparency. We reward and reinvest in our community. We encourage innovation and creativity, allowing us to cultivate interoperability from the ground up. Our collective efforts will act as a community incubator, allowing abstract ideas to transition toward economically viable projects. Community participation and DAO voting will direct our treasury toward supporting promising creators within the Astar ecosystem. Whether you are an artist, creator, influencer, developer, degen, or ape; all are welcome, and all will be rewarded for their constructive contributions.   We believe our platform will create a thriving ecosystem, by incentivizing individual participation. Our DAO is a powerful tool needed to bring new ideas forward, and to build a robust future for the interoperable block-chain space.   We are relentless. We work as one. We are Astar Degens.`,
+				extra: 'Defi, DEX',
 				media: 'https://bafybeifqn5sfmyv6edoj4tp2saxmx2wv7whdgcgijx76xefo5iplfbwn6y.ipfs.nftstorage.link/Screen%20Shot%202022-06-14%20at%2022.16.36.png',
 			},
 		},
@@ -108,8 +108,8 @@ export const DAPP__DATA = [
 		itemData: {
 			metadata: {
 				title: 'ADAO',
-				description: 'description',
-				extra: 'extra',
+				description: `About ADAOA-DAO will be based on dApp staking of Astar Network. Users will get some of the developer rewards while participating and gaining basic rewards. At present, A-DAO divides the developer rewards into: Revenue Reward, On-chain Treasury, Incubation Fund, any rewards of which can be adjusted by DAO governance. Revenue Reward60% of developer revenue will be distributed to all users who participate in staking Astar, for A-DAO itself was created to allow everyone to get more income. On-chain Treasury 30% of the developer revenue will be allocated to the on-chain treasury, which will first be used to invest in ecological projects on Astar, including but not limited to: project investment, liquidity mining, etc. Incubation Fund 10% of the earliest developer revenue will be allocated to the project side. After the establishment of the DAO committee, any builders who are conducive to ecological development can apply for this fund.`,
+				extra: 'Social, Defi',
 				media: 'https://bafybeiedodqw24b6zkpfycj2nhqil75a5dughdrj7qfiv3sfjslmrb3kle.ipfs.nftstorage.link/Screen%20Shot%202022-06-14%20at%2005.13.29.png',
 			},
 		},
@@ -123,8 +123,8 @@ export const DAPP__DATA = [
 		itemData: {
 			metadata: {
 				title: 'SushiTop',
-				description: 'description',
-				extra: 'extra',
+				description: `SUSHI TOP MARKETING is an NFT distributing specific company. Aiming to Create a culture of Token-graph marketing. About Sushi Top services Various methods of NFTdistributing Audio Token Distributor We can distribute NFTs via sound. NFT touch Shot We can send NFTs by touching an IC card to the smartphone. QR code Shot We can distribute NFTs by scanning the QR code.You can even use this in zoom MTG. You can try Audio Token distributor DEMO. https://www.youtube.com/watch?v=zYAo8ZRno1k Our Uniqueness Temporary Wallet Temporary Wallet technology allows users who do not have a wallet to receive NFTs. NFT Sealer NFT sealer technology can seal one on-chain NFT in one QR code (or URL). So enterprises can sell NFTs in real money or other digital payment methods. NFT Vending Machines We are making an actual Vending machine with some makers.It is currently being made and will be placed in movie theaters and commercial facilities. ### SUSHI TOP Vision Token Graph Marketing SUSHI TOP aims to create a culture of Token Graph Marketing. First of all, Token Graph is a concept that people can predict each person's preferences based on what kind of NFTs they have in their wallets. And we can send NFTs as Advertisements to particular NFT holders with high affinity. Examples: We sent commemorative participation NFTs only to those who attended Vtuber live music events.We sent out advertising NFTs at the time of the DVD/archival distribution release to make people aware of the DVD/archival distribution release. Marketing of Digital Ownership We believe Web3.0 is the era of digital ownership. So token-graph marketing is a marketing philosophy based on what people own in their Wallet. And this marketing cares more about privacy, we do not need cookies and other personal information. Our records on Astar and Shiden Network We distributed NFTs at a Vtuber's live performance (live venue & online distribution) with BGM. We set up NFT cards at an NFT art exhibition and distributed commemorative NFTs with them. We distributed commemorative NFTs with NFT cards and NFT Sealer at STEPN offline events. We collaborated with Horie to distribute NFTs commemorating the first anniversary of the Japanese distribution service. We distributed original NFTs of Hideki Matsui and Miliyah Kato.`,
+				extra: 'NFT, Marketplace',
 				media: 'https://bafybeibenfttro7bl3fgsk4gl3fez2s7ptweffa2q76wrtvkffbj2jeyg4.ipfs.nftstorage.link/Screen%20Shot%202022-06-14%20at%2014.45.33.png',
 			},
 		},
@@ -138,8 +138,8 @@ export const DAPP__DATA = [
 		itemData: {
 			metadata: {
 				title: 'tofuNFT',
-				description: 'description',
-				extra: 'extra',
+				description: `tofuNFT is a permissionless, decentralized and full-featured NFT marketplace. We provide numerous creative improvements to the existing marketplace experience like incentivized bidding, real-time notification, attributes filtering, bulk listing, and rarity explorer. tofuNFT supports 25+ multiple chains, BSC, Ethereum, Avalanche, Polygon, Fantom, Arbitrum, Optimism and so on. We are not only BSC, Arbitrum, Metis, Astar, Moonbeam and Harmony’s No. 1 with a trading volume NFT Marketplace, but also top rank volume on several chains.`,
+				extra: 'NFT, Utilities, Social',
 				media: 'https://bafybeifawbe3ksczvz5dsunmqwkljce2nwe4xbeszbrc6teat5xz6p4h3a.ipfs.nftstorage.link/Screen%20Shot%202022-06-14%20at%2014.17.53.png',
 			},
 		},
@@ -153,8 +153,8 @@ export const DAPP__DATA = [
 		itemData: {
 			metadata: {
 				title: 'Astar Cats',
-				description: 'description',
-				extra: 'extra',
+				description: `AstarCats is the first cats NFT collection on Astar Network. AstarCats is not just another PFP project, but it comes with many utilities and holder benefits planned ahead! Our ultimate mission is to support promising projects on Astar Network by providing our creative resources. The art is CC0(Creative Common zero), which means you will own all rights to the NFT you mint/buy. This will allow you or any projects to use AstarCats brand and help create AstarCats ecosystem.`,
+				extra: 'NFT',
 				media: 'https://bafybeihumkfixgyh43jqapvuq6gse4vs2rtclnbr2pwfxmsdg6ykplh2a4.ipfs.nftstorage.link/Screen%20Shot%202022-06-14%20at%2014.32.50.png',
 			},
 		},
