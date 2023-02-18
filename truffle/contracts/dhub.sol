7b605c1b12a0c883a19a6dd31edfec4b3ac08fce
Timothy1102 <thang.vu@kobizo.de>
2023-02-18 11:01:55 +0700

    init truffle

---
+++ truffle/contracts/dhub.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+error TicketNotExist();
+error InvalidLengthError();
+
+contract NftTicket is Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    // using IPFS or not?
+    struct App {
+        uint256 id;
+        string name;
+        string description;
+        string image;
+        string bytecode;
+    }
+
+    App[] public apps;
+
+    mapping(address => App[]) public userToApps;
+
+    function creatApp(
+        uint256 id,
+        string memory name,
+        string memory description,
+        string memory image,
+        string memory bytecode
+    ) external onlyOwner {
+        apps.push(App(id, name, description, image, bytecode));
+    }
+
+    /**
+     * @notice Get all the listed offer with status SELLING
+     * @return Array of offer ids that are listed with status SELLING
+     */
+    function getAllPurchasableMarketItems() external view returns (App[] memory) {
+        return apps;
+    }
+    
+}
