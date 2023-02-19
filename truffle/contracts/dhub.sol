670a16dde01d7651afc59672a7ab92423d7eb7ec
Timothy1102 <thang.vu@kobizo.de>
2023-02-19 17:35:02 +0700

    add privacy to contract when viewing app info

--- truffle/contracts/dhub.sol
+++ truffle/contracts/dhub.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 error AppNotExist();
+error NotAllowedToSeeAppInfo();
 
 contract DHubStore is Ownable {
     using Counters for Counters.Counter;
@@ -19,9 +20,10 @@ contract DHubStore is Ownable {
         string bytecode;
     }
 
-    App[] public apps;
+    App[] private apps;
 
-    mapping(address => App[]) private userToApps;
+    mapping(uint => address) public appIdToOwnerAddressMapper;
+    mapping(address => App[]) private userToAppsMapper;
 
     function creatApp(
         string memory name,
@@ -32,16 +34,18 @@ contract DHubStore is Ownable {
         uint256 appId = _idCounter.current();
         _idCounter.increment();
         apps.push(App(appId, name, description, image, bytecode));
+        appIdToOwnerAddressMapper[appId] = msg.sender;
     }
 
     /**
+     * @notice it is free at the moment to use an app, no fee is charged when user use apps
      * @dev user request to use an app
      */
     function useApp(uint256 appId) external {
         if (appId >= apps.length) revert AppNotExist();
         for (uint i = 0; i < apps.length; i++) {
             if (apps[i].id == appId) {
-                App[] storage userApps = userToApps[msg.sender];
+                App[] storage userApps = userToAppsMapper[msg.sender];
                 userApps.push(apps[i]);
             }
         }
@@ -51,15 +55,17 @@ contract DHubStore is Ownable {
      * @dev Get all listed apps
      * @return Array of Apps that are listed
      */
-    function getAllPurchasableMarketItems() external view returns (App[] memory) {
+    function getAllPurchasableMarketItems() external view onlyOwner returns (App[] memory) {
         return apps;
     }
 
     /**
-     * @dev Get all apps belong to a user
+     * @dev Get all apps belong to a user (apps that the user has used)
+     * @notice Only app users can view their own app info
      * @return Array of Apps
      */
-    function getAppsBelongToUser() external view returns (App[] memory) {
-        return userToApps[msg.sender];
+    function getAppsBelongToUser(address appUserAddress) external view returns (App[] memory) {
+        if (msg.sender != appUserAddress) revert NotAllowedToSeeAppInfo();
+        return userToAppsMapper[appUserAddress];
     }
 }
